b27ed6e92499e51892cea806c555f668b7256574
assyaref <syarif.revolusion@icloud.com>
2026-06-11 15:30:58 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,11 +2,17 @@ const CONFIG = {
 
     APP_NAME: "Building Care System",
 
-    VERSION: "2.0.0",
+    VERSION: "3.0.0",
 
     API_URL:
     "https://script.google.com/macros/s/AKfycbzsXdwNtcAVEtfNqk41L423XRcBJFfAm4I0K0Kf2fp_y9vbFSysSGLkMZLRzUF7QuWDSg/exec",
 
-    SESSION_KEY: "BCS_USER"
+    STORAGE: {
+
+        USER: "BCS_USER",
+
+        TOKEN: "BCS_TOKEN"
+
+    }
 
 };
