01f413515bee85855e11aeb4be806bd7b2f650c7
assyaref <syarif.revolusion@icloud.com>
2026-06-11 15:21:37 +0700

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,11 +1,49 @@
-formatDate()
+function loading(button,text){
 
-formatTime()
+    button.disabled=true;
 
-capitalize()
+    button.innerHTML=
 
-numberFormat()
+    `<span class="spinner-border spinner-border-sm"></span> ${text}`;
 
-loading()
+}
 
-hideLoading()
+function resetButton(button,text){
+
+    button.disabled=false;
+
+    button.innerHTML=text;
+
+}
+
+function formatDate(date){
+
+    return new Date(date).toLocaleString("id-ID");
+
+}
+
+function badge(status){
+
+    switch(status){
+
+        case "OPEN":
+
+            return
+
+            '<span class="badge bg-danger">OPEN</span>';
+
+        case "PROGRESS":
+
+            return
+
+            '<span class="badge bg-warning">PROGRESS</span>';
+
+        case "DONE":
+
+            return
+
+            '<span class="badge bg-success">DONE</span>';
+
+    }
+
+}
