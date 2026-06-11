bb42e7b88beacef8a9aa73b8feb34ee24c7e96f4
assyaref <syarif.revolusion@icloud.com>
2026-06-11 10:32:37 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,39 @@
+function saveSession(user) {
 
+    sessionStorage.setItem(
+
+        CONFIG.SESSION_KEY,
+
+        JSON.stringify(user)
+
+    );
+
+}
+
+function getSession() {
+
+    return JSON.parse(
+
+        sessionStorage.getItem(CONFIG.SESSION_KEY)
+
+    );
+
+}
+
+function logout() {
+
+    sessionStorage.removeItem(CONFIG.SESSION_KEY);
+
+    window.location.href = "index.html";
+
+}
+
+function checkLogin() {
+
+    if (!getSession()) {
+
+        window.location.href = "index.html";
+
+    }
+
+}
