f16ff8f3dd8739aa7767243ba2867f8ebc3a394a
assyaref <syarif.revolusion@icloud.com>
2026-06-11 11:32:54 +0700

    Update API_URL to new endpoint

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@ const CONFIG = {
     VERSION: "2.0.0",
 
     API_URL:
-        "https://script.google.com/macros/s/AKfycbywiN8W5CP0uW-bgtcCvTlNQmEI_Hru6l9MJwO6xsE2h_RClO51Vd0HfmkHBcGme2ElkA/exec",
+    "https://script.google.com/macros/s/AKfycbzsXdwNtcAVEtfNqk41L423XRcBJFfAm4I0K0Kf2fp_y9vbFSysSGLkMZLRzUF7QuWDSg/exec",
 
     SESSION_KEY: "BCS_USER"
 
