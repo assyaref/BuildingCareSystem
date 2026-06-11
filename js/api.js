874e6e7b12cf11f0093df49634f7416691ca1ca5
assyaref <syarif.revolusion@icloud.com>
2026-06-11 11:33:23 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,27 +1,41 @@
-const API = CONFIG.API_URL;
+async function api(action, data = {}) {
 
-async function api(action,data={}){
+    try {
 
-    const response = await fetch(API,{
+        const response = await fetch(CONFIG.API_URL, {
 
-        method:"POST",
+            method: "POST",
 
-        headers:{
+            headers: {
 
-            "Content-Type":"text/plain;charset=utf-8"
+                "Content-Type": "text/plain;charset=UTF-8"
 
-        },
+            },
 
-        body:JSON.stringify({
+            body: JSON.stringify({
 
-            action,
+                action,
 
-            ...data
+                ...data
 
-        })
+            })
 
-    });
+        });
 
-    return await response.json();
+        return await response.json();
+
+    }
+
+    catch (err) {
+
+        return {
+
+            success: false,
+
+            message: err.message
+
+        };
+
+    }
 
 }
