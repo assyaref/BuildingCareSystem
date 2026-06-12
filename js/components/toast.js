90105aaa42873829978a11f16aaa3eea85fdfa4d
assyaref <syarif.revolusion@icloud.com>
2026-06-12 16:03:39 +0700

    Add loader overlay styles to toast component

--- js/components/toast.js
+++ js/components/toast.js
@@ -1,1 +1,25 @@
+.loader-overlay{
 
+    position:fixed;
+
+    top:0;
+
+    left:0;
+
+    width:100%;
+
+    height:100%;
+
+    background:rgba(255,255,255,.7);
+
+    display:flex;
+
+    justify-content:center;
+
+    align-items:center;
+
+    z-index:99999;
+
+    backdrop-filter:blur(3px);
+
+}
