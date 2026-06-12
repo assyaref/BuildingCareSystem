4ab41641c877aa02409072e0682dfaa2cdae271a
assyaref <syarif.revolusion@icloud.com>
2026-06-12 16:02:57 +0700

    Update loader.js

--- js/components/loader.js
+++ js/components/loader.js
@@ -1,1 +1,41 @@
+const Loader = {
 
+    show() {
+
+        let loader = document.getElementById("globalLoader");
+
+        if (!loader) {
+
+            loader = document.createElement("div");
+
+            loader.id = "globalLoader";
+
+            loader.innerHTML = `
+
+                <div class="loader-overlay">
+
+                    <div class="spinner-border text-primary"></div>
+
+                </div>
+
+            `;
+
+            document.body.appendChild(loader);
+
+        }
+
+    },
+
+    hide() {
+
+        const loader = document.getElementById("globalLoader");
+
+        if (loader) {
+
+            loader.remove();
+
+        }
+
+    }
+
+};
