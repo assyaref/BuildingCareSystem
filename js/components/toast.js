cab4fe896d355961cc247bbd85b81b39238d1a17
assyaref <syarif.revolusion@icloud.com>
2026-06-12 16:04:35 +0700

    Update toast.js

--- js/components/toast.js
+++ js/components/toast.js
@@ -25,3 +25,48 @@ const Toast = {
     }
 
 };
+
+// CSS
+.toast-custom{
+
+    position:fixed;
+
+    right:20px;
+
+    top:20px;
+
+    background:#198754;
+
+    color:white;
+
+    padding:14px 22px;
+
+    border-radius:10px;
+
+    opacity:0;
+
+    transition:.3s;
+
+    z-index:99999;
+
+}
+
+.toast-custom.show{
+
+    opacity:1;
+
+}
+
+.toast-custom.danger{
+
+    background:#dc3545;
+
+}
+
+.toast-custom.warning{
+
+    background:#ffc107;
+
+    color:#000;
+
+}
